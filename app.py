e54b7f10d3c4ced117c0622b9376117e19047c05
Nehal <nehaltangudu12@gmail.com>
2023-04-30 13:15:41 -0500

    added progress bar to frontend

--- app.py
+++ app.py
@@ -7,10 +7,23 @@ import base64
 import os
 import uuid
 from flask import Flask, jsonify, render_template, request
+import threading
+from flask_socketio import SocketIO
 
 mg_ports = {}
+response = []
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
+lock = threading.Lock()
+def threadMMG(mg_url,base_img_name,tilesAcross,renderedTileSize):
+    req = requests.post(f'{mg_url}?tilesAcross={tilesAcross}&renderedTileSize={renderedTileSize}',
+                files={"image": open(base_img_name, "rb")}
+            )
+    global response
+    with lock:
+        response += req.json()
 
 @app.route("/", methods=["GET"])
 def GET_index():
@@ -33,6 +46,7 @@ def PUT_addMMG():
 @app.route("/makeMosaic", methods=["POST"])
 def POST_makeMosaic():
     """Route to generate mosaic"""
+    global response
     response = []
     try:
         start_time = time.time()
@@ -41,20 +55,26 @@ def POST_makeMosaic():
         filetype = input_file.filename.split(".")[-1]
         base_img_name = f"temp-{uuid.uuid4()}.{filetype}"
         input_file.save(base_img_name)
-
+        tasks = []
         for idx, (theme, mg_url) in enumerate(mg_ports.items(), 1):
             print(f"Generating {theme} mosiac ({idx}/{len(mg_ports)})")
-            req = requests.post(
-                f'{mg_url}?tilesAcross={request.form["tilesAcross"]}&renderedTileSize={request.form["renderedTileSize"]}',
-                files={"image": open(base_img_name, "rb")}
-            )
-            response += req.json()
+            t = threading.Thread(target=threadMMG,args=(mg_url,base_img_name,request.form["tilesAcross"],request.form["renderedTileSize"]))
+            tasks.append(t)
+            t.start()
+        while not (len(response) == len(mg_ports)):
+            time.sleep(1)
+            socketio.emit("progress update",str(len(response)/len(mg_ports)))
 
+        for t in tasks:
+            t.join()
+            
         os.system(f"rm {base_img_name}")
         print(
             f"Spent {time.time() - start_time} seconds to generate {len(mg_ports)} images"
         )
-    except:
+    
+    except Exception as e:
+        print(e)
         with open("static/favicon.png", "rb") as f:
             buffer = f.read()
             b64 = base64.b64encode(buffer)
--- static/js/1989.js
+++ static/js/1989.js
@@ -7,21 +7,30 @@ let doSubmit = function () {
   data.append("image", f);
   data.append("tilesAcross", tilesAcross);
   data.append("renderedTileSize", renderedTileSize);
+  var socket = io();
+  socket.on('progress update', function (progress) {
+    console.log("progress" + progress);
+    progbar = document.getElementById("progbar");
+    console.log(progbar.innerText)
+    progbar.style.width = String(Math.round(Number(progress * 100))) + '%';
+    progbar.setAttribute('aria-valuenow', Math.round(Number(progress * 100)));
+    progbar.textContent = String(Math.round(Number(progress * 100))) + '%';
+  });
   fetch("/makeMosaic", {
     method: "POST",
     body: data,
   })
-  .then((response) => response.json())
-  .then((json) => {
-    let html = "";
+    .then((response) => response.json())
+    .then((json) => {
+      let html = "";
 
-    html += `<div class="row">`;
-    for (let d of json) {
-      html += `<div class="col-2"><img src="${d.image}" class="img-fluid"></div>`;
-    }
-    html += `</div>`;
+      html += `<div class="row">`;
+      for (let d of json) {
+        html += `<div class="col-2"><img src="${d.image}" class="img-fluid"></div>`;
+      }
+      html += `</div>`;
 
-    let e = document.getElementById("mosaics");
-    e.innerHTML = html;
-  });
+      let e = document.getElementById("mosaics");
+      e.innerHTML = html;
+    });
 };
